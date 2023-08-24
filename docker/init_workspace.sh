6ed2b5baf328553b5ed6ffffb5a5dc8fc2b93932
ArghyaChatterjee <arghyame20buet@gmail.com>
2023-08-23 20:32:28 -0500

    modified the code to use on upgraded pc

---
+++ docker/init_workspace.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+
+# Stop in case of any error.
+set -e
+
+source /opt/ros/noetic/setup.bash
+
+# Create catkin workspace.
+mkdir -p ${CATKIN_WS}/src
+cd ${CATKIN_WS}/src
+catkin_init_workspace
+cd ..
+catkin_make
